35d485fb0f84531ddab8b88ed8cd0bc72fd79703
sbutler18 <122946750+sbutler18@users.noreply.github.com>
2024-03-05 10:52:05 -0500

    Update ForwardDADA2.R

--- ForwardDADA2.R
+++ ForwardDADA2.R
@@ -19,13 +19,11 @@ packageVersion("Biostrings")
 library("DECIPHER")
 library(decontam)
 
-
 #set your path
 path = "/Users/sterling.butler/Downloads/240209_M02476_0584_000000000-LCRB7/Alignment_1/20240211_215111/Fastq"
 
 # one holding the file names of all the forward reads
 fnFs <- sort(list.files(path, pattern = "_L001_R1_001.fastq.gz", full.names = TRUE))
-
 
 #what primeres were used in your study (if you can't find the study, just use 515F and 806R 
 #(FWD:GTGYCAGCMGCCGCGGTAA; REV:GGACTACNVGGGTWTCTAAT)
@@ -52,8 +50,7 @@ FWD.orients
 
 fnFs.filtN <- file.path(path, "filtN", basename(fnFs)) # Put N-filterd files in filtN/ subdirectory
 
-
-#Creating output directory: /Volumes/ROCKET-nano/TMm/PRJNA413177//filtN
+#Creating output directory:
 #this is going to take awhile
 filterAndTrim(fnFs, fnFs.filtN, maxN = 0, multithread = TRUE)
 
@@ -65,11 +62,9 @@ primerHits <- function(primer, fn) {
 rbind(FWD.ForwardReads = sapply(FWD.orients, primerHits, fn = fnFs.filtN[[1]]), 
       REV.ForwardReads = sapply(REV.orients, primerHits, fn = fnFs.filtN[[1]]))
 
-
 #direct them to cutadapt
 cutadapt <- "/Users/sterling.butler/miniconda3/envs/cutadaptenv/bin/cutadapt"
 system2(cutadapt, args = "--version") # Run shell commands from R
-
 
 #now time to remove the primers; oof this goes on and on
 path.cut <- file.path(path, "cutadapt") #adds a directory for the trimmed reads
@@ -83,14 +78,12 @@ REV.RC <- dada2:::rc(REV)
 # Trim FWD and the reverse-complement of REV off of R1 (forward reads)
 R1.flags <- paste("-g", FWD, "-a", REV.RC) 
 
-
 # Run Cutadapt
 for(i in seq_along(fnFs)) {
   system2(cutadapt, args = c(R1.flags,  "-n", 2, # -n 2 required to remove FWD and REV from reads
                              "-o",  fnFs.cut[i], # output files
                              fnFs[i])) # input files
 }
-
 
 #let's check if it works 
 rbind(FWD.ForwardReads = sapply(FWD.orients, primerHits, fn = fnFs.cut[[1]]), 
@@ -104,11 +97,9 @@ rbind(FWD.ForwardReads = sapply(FWD.orients, primerHits, fn = fnFs.cut[[1]]),
 #REV.ForwardReads       0          0       0       0
 #REV.ReverseReads       0          0       0       0
 
-
 #they are now trimmed and in a folder called cutadapt
 # Forward and reverse fastq filenames have the format:
 cutFs <- sort(list.files(path.cut, pattern = "_L001_R1_001.fastq.gz", full.names = TRUE))
-
 
 # Extract sample names, assuming filenames have format:
 get.sample.name <- function(fname) strsplit(basename(fname), "_")[[1]][1]
@@ -157,14 +148,7 @@ head(taxa.print)
 
 getwd()
 setwd("/Users/sterling.butler/Desktop")
-
-#alternative way to assign taxa with silva database
-## downloading DECIPHER-formatted SILVA v138 reference
-download.file(url="http://www2.decipher.codes/Classification/TrainingSets/SILVA_SSU_r138_2019.RData", destfile="SILVA_SSU_r138_2019.RData")
-
-## loading reference taxonomy object
-load("SILVA_SSU_r138_2019.RData")
-
+write.table(taxa.print, "ASVs_taxonomy.tsv", sep = "\t", quote=F, col.names=NA)
 
 ## creating DNAStringSet object of our ASVs
 dna <- DNAStringSet(getSequences(seqtab.nochim))
@@ -191,18 +175,6 @@ write(asv_fasta, "ASVs.fa")
 asv_tab <- t(seqtab.nochim)
 row.names(asv_tab) <- sub(">", "", asv_headers)
 write.table(asv_tab, "ASVs_counts.tsv", sep="\t", quote=F, col.names=NA)
-
-# tax table:
-# creating table of taxonomy and setting any that are unclassified as "NA"
-ranks <- c("domain", "phylum", "class", "order", "family", "genus", "species")
-asv_tax <- t(sapply(tax_info, function(x) {
-  m <- match(ranks, x$rank)
-  taxa <- x$taxon[m]
-  taxa[startsWith(taxa, "unclassified_")] <- NA
-  taxa
-}))
-colnames(asv_tax) <- ranks
-rownames(asv_tax) <- gsub(pattern=">", replacement="", x=asv_headers)
 
 write.table(taxa.print, "ASVs_taxonomy.tsv", sep = "\t", quote=F, col.names=NA)
 
